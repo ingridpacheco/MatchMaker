f7fd32a0403510e8709e85b25196c6988bfc9201
Ingrid Pacheco <ingrid.pacheco@corp.globo.com>
2020-08-16 20:52:26 -0300

    feat: improve match interface and match algorithm

--- match.py
+++ match.py
@@ -1,6 +1,8 @@
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import QKeySequence, QPalette, QColor
+from match_interface import Match_Interface
 from PyQt5.QtCore import Qt
+import sys
 
 class Match:
     def __init__(
@@ -11,63 +13,42 @@ class Match:
 
     def check_match(self, student, mentor):
         match = 0
+        total = 0
         for idx, prop_value in enumerate(mentor.get_vital_properties()):
-            # print(student.get_property(idx))
-            # print(mentor.get_property(idx))
-            if (student.get_property(idx) == mentor.get_property(idx)):
-                match += 25
+            mentor_props = str(prop_value).split(',')
+            total += len(mentor_props)
+            student_props = str(student.get_property(idx)).split(',')
+
+            match_props = set(mentor_props).intersection(student_props)
+
+            match += len(match_props)
+
+        percentage = match * 100 / total
         
         actual_list = self.list_of_matches.get(mentor.get_name())
         if actual_list == None:
-            self.list_of_matches[mentor.get_name()] = {student.get_name(): match}
+            self.list_of_matches[mentor.get_name()] = {student.get_name(): percentage}
         else:
-            self.list_of_matches[mentor.get_name()][student.get_name()] = match
+            self.list_of_matches[mentor.get_name()][student.get_name()] = percentage
 
-        self.create_interface(student, mentor)
+        if (percentage > 60):
+            app = QApplication([])
+            app = self.define_style(app)
+            
+            match_app = Match_Interface(student, mentor, app)
+            match_app.show()
+            app.exec()
 
-    def create_interface(self, student, mentor):
-        self.app = QApplication([])
-        self.app.setStyle("Fusion")
+    def define_style(self, app):
+        app.setStyle("Fusion")
 
         palette = QPalette()
         palette.setColor(QPalette.Window, QColor(53, 53, 53))
         palette.setColor(QPalette.WindowText, Qt.white)
 
-        self.app.setPalette(palette)
-
-        self.app.setApplicationName("Match found!")
-
-        topLeftGroupBox = QGroupBox("Group 1")
-
-        layout = QVBoxLayout()
-
-        student_info = QLabel(student.get_name())
-        student_info.show()
-
-        mentor_info = QLabel(mentor.get_name())
-        mentor_info.show()
-
-        yes_button = QPushButton('Sim')
-        yes_button.clicked.connect(self.on_button_clicked)
-        no_button = QPushButton('Não')
-        no_button.clicked.connect(self.on_button_clicked)
-
-        layout.addWidget(student_info)
-        layout.addWidget(mentor_info)
-        layout.addWidget(yes_button)
-        layout.addWidget(no_button)
-
-        topLeftGroupBox.setLayout(layout)
-
-        topLeftGroupBox.show()
-        self.app.exec_()
-
-    def on_button_clicked(self):
-        self.app.quit()
-        # alert = QMessageBox()
-
-        # alert.setText('sim')
-        # alert.exec_()
+        app.setPalette(palette)
+        app.setApplicationName("Match found!")
+        return app
 
     def get_list_of_matches(self):
         return self.list_of_matches
---
+++ match_interface.py
@@ -0,0 +1,83 @@
+from PyQt5.QtWidgets import *
+
+class Match_Interface(QDialog):
+    def __init__(
+        self,
+        student,
+        mentor,
+        app
+    ):
+
+        self.student = student
+        self.mentor = mentor
+        self.app = app
+
+        super(Match_Interface, self).__init__()
+
+        self.create_student()
+        self.create_mentor()
+        self.create_buttons()
+
+        mainLayout = QGridLayout()
+        mainLayout.addWidget(self.studentGroupBox, 0, 0)
+        mainLayout.addWidget(self.mentorGroupBox, 0, 1)
+        mainLayout.addLayout(self.bottomLayout, 1, 0, 1, 2)
+        mainLayout.setRowStretch(1, 1)
+        mainLayout.setRowStretch(2, 1)
+        mainLayout.setColumnStretch(0, 1)
+        mainLayout.setColumnStretch(1, 1)
+        self.setLayout(mainLayout)
+
+    def create_buttons(self):
+        self.bottomLayout = QHBoxLayout()
+
+        yes_button = QPushButton('Sim')
+        yes_button.clicked.connect(self.accept_match)
+        no_button = QPushButton('Não')
+        no_button.clicked.connect(self.reject_match)
+
+        self.bottomLayout.addWidget(yes_button)
+        self.bottomLayout.addWidget(no_button)
+        self.bottomLayout.addStretch(1)
+
+    def create_student(self):
+        self.studentGroupBox = QGroupBox("Student")
+
+        student_info = QLabel(self.student.get_name())
+        student_info.show()
+
+        layout = QVBoxLayout()
+        layout.addWidget(student_info)
+        layout = self.render_properties(self.student.get_vital_properties(), layout)
+        layout.addStretch(1)
+
+        self.studentGroupBox.setLayout(layout)
+
+    def create_mentor(self):
+        self.mentorGroupBox = QGroupBox("Mentor")
+
+        mentor_info = QLabel(self.mentor.get_name())
+        mentor_info.show()
+
+        layout = QVBoxLayout()
+        layout.addWidget(mentor_info)
+        layout = self.render_properties(self.mentor.get_vital_properties(), layout)
+        layout.addStretch(1)
+
+        self.mentorGroupBox.setLayout(layout)
+
+    def accept_match(self):
+        self.app.quit()
+
+    def reject_match(self):
+        self.app.quit()
+
+    def render_properties(self, properties, layout):
+        for property_info in properties:
+            property_label = QLabel(str(property_info))
+            property_label.show()
+
+            layout.addWidget(property_label)
+        
+        return layout
+
