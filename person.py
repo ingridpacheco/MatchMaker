91ebc95fdc3d288c535804c74e594dc3f6a1ff32
Ingrid Pacheco <ingrid.pacheco@corp.globo.com>
2020-08-21 00:38:27 -0300

    feat: improve person class to deliever properties and quantity of students

--- person.py
+++ person.py
@@ -2,14 +2,30 @@ class Person:
     def __init__(
         self,
         name,
+        email,
+        phone,
         vital_properties,
         all_properties,
-        hours = None
+        quantity_students,
+        additional_properties = []
     ):
         self.name = name
+        self.email = email
+        self.phone = phone
         self.vital_properties = vital_properties
         self.all_properties = all_properties
-        self.hours = hours
+        self.quantity_students = quantity_students
+        self.additional_properties = additional_properties
+
+    def get_information(self, type):
+        information = []
+        information.append(self.name),
+        information.append(self.email),
+        information.append(self.phone),
+        information.extend(self.vital_properties)
+        if (type == "student"):
+            information.extend(self.additional_properties)
+        return information
 
     def get_name(self):
         return self.name
@@ -23,5 +39,8 @@ class Person:
     def get_all_properties(self):
         return self.all_properties
 
-    def get_hours(self):
-        return self.hours
+    def get_quantity_students(self):
+        return self.quantity_students
+
+    def decrease_quantity_students(self):
+        self.quantity_students -= 1
