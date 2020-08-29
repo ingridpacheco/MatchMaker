92dc85b01973a2fbda8c7d9b281fda22c4325668
Ingrid Pacheco <ingrid.pacheco@corp.globo.com>
2020-08-29 20:28:04 -0300

    feat: add function to get name of group of people as list

--- people.py
+++ people.py
@@ -1,9 +1,6 @@
 class People:
-    def __init__(
-        self,
-        list_of_person = []
-    ):
-        self.list_of_person = list_of_person
+    def __init__(self):
+        self.list_of_person = []
 
     def get_person(self, name):
         for person in self.list_of_person:
@@ -13,5 +10,14 @@ class People:
 
     def add_person(self, person):
         self.list_of_person.append(person)
+
+    def get_list(self):
+        return self.list_of_person
+
+    def get_names(self):
+        names = []
+        for person in self.list_of_person:
+            names.append(person.get_name())
+        return names
 
     
